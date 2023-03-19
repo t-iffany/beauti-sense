6074a53b96a5cb0734da10e3858d8fcabef41535
t-iffany <tiffanyxleong@gmail.com>
2023-03-18 19:17:53 -0700

    Update npm dependencies. Integrate opencv flask face mesh video feed into react component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import VideoFeed from './VideoFeed';
 
 function App() {
   return (
     <div className="App">
       <h1>BeautiSense</h1>
+      <VideoFeed />
     </div>
   );
 }
