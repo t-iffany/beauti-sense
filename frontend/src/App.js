d5b59b0b6b9f70625c4135e03d0be7885661bdaa
Bianca Fu <0509biancafu@gmail.com>
2023-03-19 19:53:35 -0600

    generate diff ai-comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,30 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState("");
+  const [counter, setCounter] = useState(1);
   const [currentWord, setCurrentWord] = useState("");
 
   function handleClick() {
-    const generateText = 'According to the data collected, it appears that your lipstick did not fully cover your entire lips.'
+    
+    setCounter(counter + 1); 
+    let generateText = "";
+    switch(counter) {
+      case 1:
+        generateText = 'According to the data collected, it appears that your lipstick did not fully cover your entire lips.'
+        break;
+      case 2:
+        generateText = 'Your make up looks good, you look amazing!'
+        break;
+      default:
+        generateText = ""
+    }
     setText(generateText);
   }
 
   useEffect(() => {
     let currentIndex = 0;
     let intervalId = null;
-
+    setCurrentWord("");
     function typeWord() {
       const words = text.split(" ");
       const nextWord = words[currentIndex];
