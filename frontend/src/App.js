b29736dd74e28a43adc985e59ff2a2cfceeb0a6b
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-19 19:24:12 -0600

    added generate text option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
 import VideoFeed from './VideoFeed';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [text, setText] = useState("");
+  const [currentWord, setCurrentWord] = useState("");
+
+  function handleClick() {
+    const generateText = 'According to the data collected, it appears that your lipstick did not fully cover your entire lips.'
+    setText(generateText);
+  }
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let intervalId = null;
+
+    function typeWord() {
+      const words = text.split(" ");
+      const nextWord = words[currentIndex];
+      setCurrentWord((prevWord) => prevWord + " " + nextWord);
+      currentIndex++
+
+      if (currentIndex >= words.length) {
+        clearInterval(intervalId);
+      }
+    }
+
+    if(text) {
+      intervalId = setInterval(typeWord, 150);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [text])
+
   return (
     <div className="App">
       <h1>BeautiSense</h1>
@@ -9,8 +40,9 @@ function App() {
         generate new look | get inspired
       </div>
       <VideoFeed />
+      <button onClick={handleClick}>Check my makeup</button>
       <div className="ai-comment">
-      <p>Writing something... </p>
+      <p>{currentWord} </p>
       <button>listen</button>    
       </div>  
     </div>
